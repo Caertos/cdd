22b03916af95b798c6a034b55524537018e49159
Caertos <cocheroc@gmail.com>
2025-10-17 16:07:24 -0500

    docs(typedef): add DockerClient typedef for jsdoc

--- src/helpers/dockerService/dockerService.js
+++ src/helpers/dockerService/dockerService.js
@@ -4,6 +4,25 @@ import Docker from "dockerode";
 // - /var/run/docker.sock on Linux/Mac
 // - //./pipe/docker_engine on Windows
 // - Environment variables DOCKER_HOST, DOCKER_CERT_PATH, etc.
+
+/**
+ * Typedef for the docker client used in this project.
+ * Use a generic object shape so JSDoc can render a meaningful type.
+ *
+ * @typedef {Object} DockerClient
+ * @property {Function} listContainers
+ * @property {Function} getContainer
+ * @property {Function} listImages
+ * @property {Function} createContainer
+ * @property {Function} pull
+ */
+
+/**
+ * Shared Docker client instance (dockerode). Consumers import { docker }
+ * and use it to list/create/start/stop containers and images.
+ *
+ * @type {DockerClient}
+ */
 const docker = new Docker();
 
 export { docker };
